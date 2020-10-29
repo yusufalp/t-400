b77331101d8c4a6e712f247a9747f6c899084862
Yusuf Alp <alpyusuf007@gmail.com>
2020-10-29 12:50:16 -0700

    Added a 404 page

--- src/Page404/Page404.js
+++ src/Page404/Page404.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 export default function Page404() {
   return (
-    <div>
-      <p>Page not found!</p>
+    <div className='title not-found'>
+      <h2>Page not found!</h2>
+      <img src='https://thumbs.gfycat.com/CarefreeWhoppingFluke-max-1mb.gif' alt='hasta la vista, baby' />
     </div>
   )
 }
